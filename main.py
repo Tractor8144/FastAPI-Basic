f511b1dd6b984f9ed3b5cc8c1cdae6db15946f32
Detergent4316 <maharshi@midasit.in>
2025-04-27 12:15:48 +0530

    Committed using GitPython

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 import fastapi as _fastapi
 from typing import TYPE_CHECKING, List
-import schemas as _schemas
+import schemas as _schema
 import sqlalchemy.orm as _orm
 import services as _services
 
 
 app = _fastapi.FastAPI()
 
+
 @app.post("/api/contacts", response_model=_schemas.Contact)
-async def create_contact(contact: _schemas.ContactCreator, db :_orm.Session = _fastapi.Depends(_services.get_db)):
+async def create_contact(contact: _schemas.ContactCreator, db: _orm.Session = _fastapi.Depends(_services.get_db)):
     return await _services.create_contact(contact, db)
